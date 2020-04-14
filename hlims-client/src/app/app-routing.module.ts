d77912a674e3e708b87982157e11677ad0b6e515
Apkolla.Swamy <Apkolla.Swamy@ASWAMY03>
2020-04-14 22:53:22 +0530

    Initial commit with routing

---
+++ hlims-client/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BorrowerComponent } from './borrower/borrower.component';
+
+
+const routes: Routes = [
+  { path: 'add-borrower', component: BorrowerComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
